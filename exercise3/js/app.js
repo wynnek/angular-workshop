0b192fdf1485398726d3a435a9b8ffc103642411
wynnek <kimberly.t.wynne@gmail.com>
2014-11-03 19:50:40 -0500

    Exercise 3 - Create a filter

--- exercise3/js/app.js
+++ exercise3/js/app.js
@@ -1,5 +1,7 @@
 // initialize the app
-angular.module('Exercise3', ['ngRoute']);
+angular.module('Exercise3.filters',[]);
+
+angular.module('Exercise3', ['ngRoute', 'Exercise3.filters']);
 
 angular.module('Exercise3').config(function($routeProvider) {
     'use strict';
@@ -25,4 +27,21 @@ angular.module('Exercise3').controller('NavbarCtrl', function($scope, $location)
     $scope.isActive = function(viewLocation) {
         return viewLocation === $location.path();
     };
+});
+
+angular.module('Exercise3.filters').filter('ransomLetter', function(){
+	return function(param){
+		var s = '';
+		if(param && param.length > 0) {
+			for(var i = 0; i < param.length; i++){
+				if( i % 2 == 0) {
+					s += param[i].toUpperCase();
+				} else {
+					s += param[i].toLowerCase();
+				}
+			}
+			s += '!'
+		}
+		return s; //param[0].toUpperCase() + param.slice(1);
+	}
 });
